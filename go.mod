1f81c045cd62914643550b8c5e14c1715b14cb74
praveen-95572 <praveen.95572@gmail.com>
2022-10-19 13:52:36 +0530

    initialise cleanup ESC cmd

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module pacman
 
 go 1.18
+
+require github.com/danicat/simpleansi v0.0.0-20200320095209-8cd0472eec8b // indirect
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+
+	"github.com/danicat/simpleansi"
 )
 
 var maze []string
@@ -25,13 +28,51 @@ func loadMaze(file string) error {
 }
 
 func printScreen() {
+	simpleansi.ClearScreen() //clean terminal
 	for _, line := range maze {
 		fmt.Println(line)
 	}
 }
 
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+
+	return "", nil
+}
+
+func initialise() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to activate cbreak mode: ", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to activate cooked mode:", err)
+	}
+}
+
 func main() {
 	//initialize game
+	initialise()
+	defer cleanup()
 
 	//load resources
 	err := loadMaze("maze01.txt")
@@ -46,15 +87,20 @@ func main() {
 		printScreen()
 
 		//process input
+		input, err := readInput()
+		if err != nil {
+			log.Println("error reading input:", err)
+			break
+		}
 
 		//process movement
 
 		//process collisions
 
 		//check game over
-
-		//Temp: break infinite loop
-		break
+		if input == "ESC" {
+			break
+		}
 
 		//repeat
 	}
